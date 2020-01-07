0571487edc3272dee9e1254daf54fac45b80a0f3
Thomas Museus Dabay <tmuseusdabay@pm.me>
2020-01-07 15:34:27 -0600

    Initial Commit

---
+++ src/links.js
@@ -0,0 +1,28 @@
+//Tile Links
+export const ANTISKEPTICISM =
+  "https://brill.com/view/book/edcoll/9789004393530/BP000007.xml";
+export const BRANDOM = "";
+export const CVGENERATOR = "https://github.com/thomas-m-d/ArbitraryCVGenerator";
+export const GOLDILOCKS =
+  "https://secularhumanism.org/2017/07/cont-goldilocks-naturalism/";
+export const INCONSISTENCY =
+  "https://www.pdcnet.org/swphilreview/content/swphilreview_2018_0034_0001_0189_0197";
+export const JUSTIFYING =
+  "https://philosophynow.org/issues/112/Justifying_Our_Moral_Judgments";
+export const KANT = "";
+export const KEYMAPS = "https://github.com/thomas-m-d/KeyboardLayouts";
+export const KNOWABILITY =
+  "http://revistas.pucsp.br/cognitiofilosofia/article/view/31233";
+export const LINUX = "";
+export const MNEMONIC =
+  "https://github.com/thomas-m-d/TeensyModular/tree/master/Mnemonic%20Recorder";
+export const PATHWAY = "";
+export const PEIRCE =
+  "https://www.tandfonline.com/doi/full/10.1080/09672559.2016.1203981";
+export const POLISH = "https://thomas-m-d.github.io/learn-polish-calc/";
+export const TRANSPOSITION =
+  "http://almukhatabatjournal.unblog.fr/2016/12/25/al-mukhatabat-journal-october-issue-162015-special-issue-on-robert-brandom/";
+
+//Contact Links
+export const GITHUB = "https://github.com/thomas-m-d/";
+export const EMAIL = "mailto:tmuseusdabay@pm.me?subject=";
